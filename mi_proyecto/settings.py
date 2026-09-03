7d73c40d1762c4ce8608896e3c40ae0d6d674150
JessicaJPR <jessipachecorodriguez@gmail.com>
2026-09-03 13:02:09 -0400

    Avance

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inicio',
-    'app1',
-    'app2',
+
 ]
 
 MIDDLEWARE = [
@@ -57,7 +56,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
